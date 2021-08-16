b81ccad0cbb8f44d14250c746132c8a017ee561e
Jeremy <jpthomas501@gmail.com>
2021-08-16 16:10:40 -0400

    day1 commit

---
+++ script.js
@@ -0,0 +1,113 @@
+let locations = {
+    yonkers: {
+        place1: {
+            name: "Afya Foundation",
+            description: "Afya Foundation is a non-profit organization that collects excess medical supplies from Hospitals in New York, and ships them to places in need of medical supplies.",
+            address: "140 Saw Mill River Rd",
+            website: "https://afyafoundation.org/",
+            image: "images/Afya Foundation Logo.png",
+        },
+        place2: {
+            name: "The Children's Village",
+            description: "The Children's Vilage is a non-profit organization with the goal of educating troubled youth so that they can successfully enter the work force.",
+            address: "179 Valentine Ln",
+            website: "https://childrensvillage.org/take-action/volunteer/",
+            image: "images/Childrens Village Logo.jpg",
+        },
+        place3: {
+            name: "Children's Cardiomyopathy Foundation",
+            description: "Children's Cardiomyopathy Foundation is a foundation dedicated to raising money towards Pediatric Cardiomyopathy, a rare heart condition.",
+          address: "Address Depends on what you volunteer for. Please visit website for more information.",
+            website: "https://www.childrenscardiomyopathy.org/pages/get-involved/volunteer/",
+            image: "images/Childrens Cardio Foundation.gif",
+        },
+        place4: {
+            name: "New York Blood Center",
+            description: "Hudson Valley Blood Service/NYBC is a Blood Center that aims to provide high quality blood and stem cells to medical facilities, conduct research in blood related medicine, and the develop products and technologies to medicine.",
+            address: "525 Executive Blvd",
+            website: "https://www.nybloodcenter.org/support-us/volunteer-nybc/volunteer-application/",
+            image: "images/NY-BLood-Center-Log.jpg",
+        },
+        place5: {
+            name: "Jay Heritage Center",
+            description: "The Jay Heritage Center is a non-profit that educates the public about the American Founding Father John Jay. This organization preserves, and interprets his belongings and documents to find more information about him, and ultimately, find out more of the history behind America.",
+            address: "210 Boston Post Road, Rye",
+            website: "https://jayheritagecenter.org/join-give/get-involved/",
+            image: "images/Jay Heritage Center.jpg",
+        },
+        place6: {
+            name: "Westchester Jewish Community Services",
+            description: "Westchester Jewish Community Services is a human resources organization that helps people beat emotional, social, vocational, and educational challenges so that they can live better lives.",
+            address: "487 S Broadway Suite 2",
+            website: "https://www.wjcs.com/ways-to-give/volunteer/",
+            image: "images/WJCS Logo.jpg",
+        },
+        place7: {
+            name: "R.A.I.N. (Regional Aid for Interim Needs)",
+            description: "R.A.I.N. is an organization that helps improve the quality of life of elderly people who have certain needs and/or disabilities.",
+            address: "720 Nereid Ave",
+            website: "https://www.raininc.org/r-a-i-n-makers%e2%80%a8%e2%80%a8/volunteer-opportunities/",
+            image: "images/RAIN Logo.png",
+        },
+    },
+
+    bronx: {
+        place1: {
+            name: "South Bronx United",
+            description: "South Bronx United is a non-profit that helps immigrant youth through education (prep classes, tutoring, etc.), legal services, and soccer. ",
+            address: "192 E 151st St 5th Floor",
+            website: "https://southbronxunited.org/volunteer",
+            image: "images/SBU Logo.png",
+        },
+        place2: {
+            name: "The Bronx River Alliance",
+            description: "The Bronx River Alliance is an organization that protects and improves the ecological condition of the Bronx River corridor so that it can be properly utilized by anyone.",
+            address: "Address Depends on what you volunteer for. Please visit website for more information.",
+            website: "https://bronxriver.org/get-involved/volunteer",
+            image: "images/Bronx River Alliance Logo.png",
+        },
+        place3: {
+            name: "",
+            description: "",
+            address: "",
+            website: "",
+            image: "",
+        },
+        place4: {
+            name: "",
+            description: "",
+            address: "",
+            website: "",
+            image: "",
+        },
+        place5: {
+            name: "",
+            description: "",
+            address: "",
+            website: "",
+            image: "",
+        },
+        place6: {
+            name: "",
+            description: "",
+            address: "",
+            website: "",
+            image: "",
+        },
+        place7: {
+            name: "",
+            description: "",
+            address: "",
+            website: "",
+            image: "",
+        },
+    },
+
+}
+
+
+/*name: "",
+            description: "",
+            address: "",
+            website: "",
+            image: "", */
